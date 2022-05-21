64e2c5f328776bff6670f60672b95a76dc0ce5cf
“jtorp” <j.kontakt234@gmail.com>
2022-05-21 23:00:57 +0200

    new style

---
+++ src/pages/Console.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Textarea from '../components/styles/Textarea';
+
+function Console() {
+   return (
+     <>
+       <Textarea />
+   </>
+  );
+}
+export default Console;
